1dc62c4c274f455b4a8064ed004379fcc3073e66
hustccc <1276675421@qq.com>
2021-02-18 05:31:52 +0900

    succeed draw text in waveshare e-paper

--- stm32h7-try/src/main.rs
+++ stm32h7-try/src/main.rs
@@ -5,35 +5,49 @@
 extern crate alloc;
 
 use alloc::{vec::Vec, vec};
-use core::fmt::Write;
-#[allow(unused_imports)]
-use core::cell::{Cell, RefCell};
 
 use cortex_m_rt::entry;
 use alloc_cortex_m::CortexMHeap;
 use panic_halt as _;
-use cortex_m::interrupt::{free, Mutex};
-use cortex_m::peripheral::NVIC;
+use spi::NoMiso;
 use stm32h7xx_hal::hal::digital::v2::OutputPin;
-use stm32h7xx_hal::{pac, prelude::*, interrupt};
-use stm32h7xx_hal::gpio::{Edge, ExtiPin, Input, Output, PullUp, PushPull};
-// Key pin
-use stm32h7xx_hal::gpio::gpioc::{PC0, PC13};
+use stm32h7xx_hal::{pac, prelude::*, spi};
+use embedded_graphics::{
+    fonts::{Font12x16, Text},
+    prelude::*,
+    text_style,
+};
+use epd_waveshare::{
+    color::*,
+    epd2in13_v2::{Display2in13, EPD2in13},
+    graphics::{Display, DisplayRotation},
+    prelude::*,
+};
+
+// use epd_waveshare::{
+//     color::*,
+//     epd2in9::{Display2in9, EPD2in9},
+//     graphics::Display,
+//     prelude::*,
+// };
 
 #[allow(unused_imports)]
 use nb::block;
 
+// RST Pin: PB11
+// BUSY Pin: PB10
+// DC Pin: PE15
+// SPI2_SCK Pin: PB13
+// SPI2_MISO Pin: PB14
+// SPI2_MOSI Pin: PB15
+// SPI2_CS1 Pin: PB12
+// SPI3_SCK Pin: PC10
+// SPI3_MISO Pin: PC11
+// SPI3_MOSI Pin: PC12
+
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
-// Semaphore for synchronization
-// static SEMAPHORE: Mutex<Cell<bool>> = Mutex::new(Cell::new(true));
-
-static KEY_PIN: Mutex<RefCell<Option<PC13<Input<PullUp>>>>> =
-    Mutex::new(RefCell::new(None));
-static LED: Mutex<RefCell<Option<PC0<Output<PushPull>>>>> =
-    Mutex::new(RefCell::new(None));
-static mut COUNTER: usize = 0;
 
 #[entry]
 fn main() -> ! {
@@ -47,7 +61,7 @@ fn main() -> ! {
     }
     assert_eq!(data, vec![0, 1, 2, 3, 4]);
 
-    let mut cp = cortex_m::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
     // Constrain and Freeze power
@@ -56,126 +70,116 @@ fn main() -> ! {
 
     // Constrain and Freeze clock
     let rcc = dp.RCC.constrain();
-    let ccdr = rcc.sys_ck(100.mhz()).freeze(pwrcfg, &dp.SYSCFG);
+    let ccdr = rcc
+        .sys_ck(100.mhz())
+        .pll1_q_ck(48.mhz())
+        .freeze(pwrcfg, &dp.SYSCFG);
 
-    let gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
-    let gpioc  = dp.GPIOC.split(ccdr.peripheral.GPIOC);
-    
+    // let gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
+    let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
+    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+
     // Configure PC0, PC1, PC2 as output.
     let mut led_0 = gpioc.pc0.into_push_pull_output();
     let mut led_1 = gpioc.pc1.into_push_pull_output();
     let mut led_2 = gpioc.pc2.into_push_pull_output();
-    let mut led_3 = gpioc.pc3.into_push_pull_output();
     
-    // Set the led down first
-    led_0.set_low().unwrap();
-    led_1.set_low().unwrap();
-    led_2.set_low().unwrap();
-    led_3.set_low().unwrap();
+    let rst = gpiob.pb11.into_push_pull_output();
+    let busy = gpiob.pb10.into_pull_down_input();
+    let dc = gpioe.pe15.into_push_pull_output();
+    let cs = gpiob.pb12.into_push_pull_output();
 
-    let tx = gpioa.pa9.into_alternate_af7();
-    let rx = gpioa.pa10.into_alternate_af7();
+    let sck = gpiob.pb13.into_alternate_af5();
+    // let miso = gpiob.pb14.into_alternate_af5();
+    let mosi = gpiob.pb15.into_alternate_af5();
 
-    let serial = dp.USART1.serial(
-        (tx, rx),
-        115_200.bps(),
-        ccdr.peripheral.USART1,
-        &ccdr.clocks
-    ).unwrap();
+    // Initialise the SPI2 peripheral.
+    let mut spi: spi::Spi<_, _, u8> = dp.SPI2.spi(
+        (sck, NoMiso, mosi),
+        spi::MODE_0,
+        3.mhz(),
+        ccdr.peripheral.SPI2,
+        &ccdr.clocks,
+    );
 
-    let (mut tx, mut _rx) = serial.split();
+    // let sck = gpioc.pc10.into_alternate_af6();
+    // let miso = gpioc.pc11.into_alternate_af6();
+    // let mosi = gpioc.pc12.into_alternate_af6();
+
+
+    // Initialise the SPI peripheral.
+    // let mut spi = dp.SPI3.spi(
+    //     (sck, miso, mosi),
+    //     spi::MODE_0,
+    //     3.mhz(),
+    //     ccdr.peripheral.SPI3,
+    //     &ccdr.clocks,
+    // );
     
-    // Get true random number generator
-    let mut rng = dp.RNG.constrain(ccdr.peripheral.RNG, &ccdr.clocks);
-    let mut random_bytes = [0u16; 3];
+    // Initialise the e-paper
+    // dc.set_low().unwrap();
+    // cs.set_low().unwrap();
+    // rst.set_high().unwrap();
 
-    rng.fill(&mut random_bytes).unwrap();
+    // cs.set_high().unwrap();
 
+    // rst.set_high().unwrap();
+    // delay.delay_ms(100_u16);
+    // rst.set_low().unwrap();
+    // delay.delay_ms(2_u16);
+    // rst.set_high().unwrap();
+    // delay.delay_ms(10_u16);
+
+    // dc.set_low().unwrap();
+    // cs.set_low().unwrap();
+    // spi.write(&[0x04]).unwrap();
+    // cs.set_high().unwrap();
+    
     // Get the delay provider.
-    #[allow(unused_variables)]
     let mut delay = cp.SYST.delay(ccdr.clocks);
 
-    // Push button configuration
-    let mut syscfg = dp.SYSCFG;
-    let mut exti = dp.EXTI;
+    // Setup the device
+    let mut epd = EPD2in13::new(
+        &mut spi, cs, busy, dc, rst, &mut delay).unwrap();
+    // Setup the graphics
+    let mut display = Display2in13::default();
+    
+    display.set_rotation(DisplayRotation::Rotate90);
 
-    let mut key = gpioc.pc13.into_pull_up_input();
-    key.make_interrupt_source(&mut syscfg);
-    key.trigger_on_edge(&mut exti, Edge::RISING);
-    key.enable_interrupt(&mut exti);
-
-    // Save information needed by the interrupt handlers to the global variable
-    free(|cs| {
-        KEY_PIN.borrow(cs).replace(Some(key));
-        LED.borrow(cs).replace(Some(led_0));
-    });
-
-    // Enable the button interrupts
-    unsafe {
-        cp.NVIC.set_priority(interrupt::EXTI15_10, 1);
-        NVIC::unmask::<interrupt>(interrupt::EXTI15_10);
-    }
-
-    // core::fmt::Write is implemented for tx.
-    writeln!(tx, "Hello, world!").unwrap();
-
+    // Draw some text
+    Text::new("Hello from Rust!", Point::new(1, 1))
+        .into_styled(text_style!(
+            font = Font12x16,
+            text_color = Black,
+            background_color = White
+        ))
+        .draw(&mut display).unwrap();
+        
+    // Transfer the frame data to the epd and display it
+    epd.update_frame(&mut spi, &display.buffer()).unwrap();
+    epd.display_frame(&mut spi).unwrap();
+    epd.sleep(&mut spi).unwrap();
+    
     loop {
-        let random_element: Result<u32, _> = rng.gen();
-        match random_element {
-            Ok(random) => {
-                match random % 3 {
-                    0 => {
-                        led_1.set_high().unwrap();
-                        delay.delay_ms(500_u16);
-                        
-                        led_1.set_low().unwrap();
-                        delay.delay_ms(500_u16);
-                    },
-                    1 => {
-                        led_2.set_high().unwrap();
-                        delay.delay_ms(500_u16);
+        led_0.set_high().unwrap();
+        delay.delay_ms(500_u16);
+        led_0.set_low().unwrap();
+        delay.delay_ms(500_u16);
 
-                        led_2.set_low().unwrap();
-                        delay.delay_ms(500_u16);
-                    },
-                    2 => {
-                        led_3.set_high().unwrap();
-                        delay.delay_ms(500_u16);
-                        
-                        led_3.set_low().unwrap();
-                        delay.delay_ms(500_u16);
-                    },
-                    _ => unreachable!()
-                }
-            },
-            Err(_) => panic!()
-        }
+        led_1.set_high().unwrap();
+        delay.delay_ms(500_u16);
+        led_1.set_low().unwrap();
+        delay.delay_ms(500_u16);
 
-        // Echo what is received on the serial link.
-        // let received = block!(rx.read()).unwrap();
-        // block!(tx.write(received)).ok();
+        led_2.set_high().unwrap();
+        delay.delay_ms(500_u16);
+        led_2.set_low().unwrap();
+        delay.delay_ms(500_u16);
+
     }
-}
 
-fn toggle_led() {
-    free(|cs| {
-        if let Some(b) = LED.borrow(cs).borrow_mut().as_mut() {
-            if unsafe { COUNTER % 2 } == 0 {
-                b.set_high().unwrap();
-            } else {
-                b.set_low().unwrap();
-            }
-        }
-    });
-    unsafe { COUNTER += 1; }
-}
-
-#[interrupt]
-fn EXTI15_10() {
-    toggle_led();
-    free(|cs| {
-        if let Some(b) = KEY_PIN.borrow(cs).borrow_mut().as_mut() {
-            b.clear_interrupt_pending_bit()
-        }
-    });
+    // Echo what is received on the serial link.
+    // let received = block!(rx.read()).unwrap();
+    // block!(tx.write(received)).ok();
 }
